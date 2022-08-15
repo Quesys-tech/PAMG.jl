bf842c380f3f74af6a58f35c4aff96fc1075fa3e
Documenter.jl <documenter@juliadocs.github.io>
2022-08-15 07:57:07 +0000

    build based on db725ad

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PAMG","category":"page"},{"location":"#PAMG","page":"Home","title":"PAMG","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PAMG.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PAMG]","category":"page"}]
+}
