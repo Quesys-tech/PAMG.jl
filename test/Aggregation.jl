453d51cb8741c3b319278ba2632d8eec1a271501
QueueSys <qdevelop@hotmail.com>
2022-08-16 00:20:26 +0900

    fix typo

--- test/Aggregation.jl
+++ test/Aggregation.jl
@@ -11,13 +11,13 @@
     push!(G, 1, 2)
     push!(G, 1, 1)
 
-    G_gs = Dict(4 => [8], 0 => [7], 2 => [6, 4, 3], 1 => [5, 2, 1], 3 => [])#ground truth
+    G_gt = Dict(4 => [8], 0 => [7], 2 => [6, 4, 3], 1 => [5, 2, 1], 3 => [])#ground truth
     for i = 0:4
         Gᵢ = Int[]
         for j in Aggregation(G, i)
             push!(Gᵢ, j)
         end
         @test eltype(Gᵢ) == Int
-        @test isempty(symdiff(G_gs[i], Gᵢ))
+        @test isempty(symdiff(G_gt[i], Gᵢ))
     end
 end
